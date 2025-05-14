7f56aa9fefd66d6af1738594edf9f5fd6d89d93e
Szymon Witt <szymonw@chalmers.se>
2025-05-14 12:36:55 +0200

    chore: adjust log messages

--- src/cv_dnn_runtime.cpp
+++ src/cv_dnn_runtime.cpp
@@ -7,24 +7,21 @@ CvDnnRuntime::CvDnnRuntime(const std::string& model_path, int trained_frame_widt
 
 void CvDnnRuntime::load() {
     auto logger = Logger::get_instance().get_logger();
-    logger->info("Loading model using CV DNN Runtime at '{}'", m_model_path);
+    logger->info("CvDnnRuntime: Loading model using at '{}'", m_model_path);
 
     try {
-        logger->info("reading from onnx");
         m_net = cv::dnn::readNetFromONNX(m_model_path);
-        logger->info("setting backend");
         m_net.setPreferableBackend(cv::dnn::DNN_BACKEND_OPENCV);
-        logger->info("setting target");
         m_net.setPreferableTarget(cv::dnn::DNN_TARGET_CPU);
     } catch (const std::exception& e) {
-        logger->error("Error while loading model using CV DNN Runtime: {}", e.what());
+        logger->error("CvDnnRuntime: Error while loading model using CV DNN Runtime: {}", e.what());
         throw;
     } catch (...) {
-        logger->error("Unknown error while loading model using CV DNN Runtime");
+        logger->error("CvDnnRuntime: Unknown error while loading model using CV DNN Runtime");
         throw;
     }
 
-    logger->info("Model loaded");
+    logger->info("CvDnnRuntime: Model loaded");
 }
 
 std::vector<cv::Mat> CvDnnRuntime::predict(const cv::Mat& image) {
