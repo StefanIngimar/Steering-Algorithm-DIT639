2d4bb00744f2e96916132cf3ab235f4fcb83cf22
Szymon Witt <szymonw@chalmers.se>
2025-05-18 01:11:20 +0200

    chore: do not mount a volume for csv file

--- run.sh
+++ run.sh
@@ -8,7 +8,6 @@ docker build -f Dockerfile -t nutmeg .
 echo "Starting nutmeg..."
 docker run --rm --net=host --ipc=host \
   -e DISPLAY="$DISPLAY" -v /tmp:/tmp \
-  -v "$(pwd)/res/steering_data/csv:/data/csv" \
   nutmeg:latest --cid=253 --name=img \
   --width=640 --height=480 --verbose
 
