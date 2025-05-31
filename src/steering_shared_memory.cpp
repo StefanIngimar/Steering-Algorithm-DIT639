18b030da538eefa2ba3e141bc6b5c9d6b901eedc
Szymon Witt <szymonw@chalmers.se>
2025-05-31 18:47:46 +0200

    chore: add logging to the steering shared memory constructor

--- src/steering_shared_memory.cpp
+++ src/steering_shared_memory.cpp
@@ -1,30 +1,37 @@
 #include "steering_shared_memory.hpp"
+#include "logger.hpp"
 #include "cluon-complete.hpp"
 
 SteeringSharedMemory::SteeringSharedMemory(key_t shm_key, size_t shm_size, key_t sem_key)
     : m_shm_key(shm_key), m_shm_size(shm_size), m_sem_key(sem_key),
       m_semid(-1), m_shmid(-1), m_is_initialized(false), m_shmaddr(nullptr) {
+
+    auto logger = Logger::get_instance().get_logger();
     
     m_semid = semget(m_sem_key, 1, IPC_CREAT | 0666);
     if (m_semid == -1) {
         throw std::runtime_error("Failed to create semaphore: " + std::string(strerror(errno)));
     }
+    logger->info("Steering Shared Memory semaphore created");
 
     semun arg;
     arg.val = 1;
     if (semctl(m_semid, 0, SETVAL, arg) == -1) {
         throw std::runtime_error("Failed to initialize semaphore: " + std::string(strerror(errno)));
     }
+    logger->info("Steering Shared Memory semaphore initialized");
 
     m_shmid = shmget(m_shm_key, m_shm_size, IPC_CREAT | 0666);
     if (m_shmid < 0) {
         throw std::runtime_error("Failed to get shared memory segment: " + std::string(strerror(errno)));
     }
+    logger->info("Steering Shared Memory shared memory received");
 
     m_shmaddr = shmat(m_shmid, nullptr, 0);
     if (m_shmaddr == (void *)-1) {
         throw std::runtime_error("Failed to attach shared memory: " + std::string(strerror(errno)));
     }
+    logger->info("Steering Shared Memory shared memory attached");
 
     m_is_initialized = true;
 }
