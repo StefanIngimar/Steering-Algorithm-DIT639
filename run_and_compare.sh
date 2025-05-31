1df06f767336990a54b8bf4a8e8c6504e1e439c9
stefanla <stefanla@student.chalmers.se>
2025-05-31 11:08:08 +0200

    feature: who knows

--- run_and_compare.sh
+++ run_and_compare.sh
@@ -10,6 +10,9 @@ REC_FILES=("$REC_DIR"/*.rec)
 echo "[*] Building Nutmeg image once..."
 docker build -f Dockerfile -t nutmeg .
 
+echo "[*] Building services..."
+docker build https://github.com/chalmers-revere/opendlv-video-h264-decoder.git#v0.0.5 -f Dockerfile -t h264decoder:v0.0.5
+
 echo "[*] Installing Python dependencies..."
 pip install -r microservices/perfy_producer/requirements.txt
 pip install -r microservices/karen/requirements.txt
