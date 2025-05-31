ede6acb7e15c39ea95460589cc8360322b779dda
Szymon Witt <szymonw@chalmers.se>
2025-05-31 15:42:52 +0200

    chore: add name of the service to logger format

--- microservices/karen/src/core/logger.py
+++ microservices/karen/src/core/logger.py
@@ -5,11 +5,12 @@ import sys
 def setup_logging() -> None:
     root_logger = logging.getLogger()
     root_logger.setLevel(logging.DEBUG)
-    
+
     root_logger.handlers.clear()
-    
+
     console_handler = logging.StreamHandler(sys.stdout)
-    console_handler.setFormatter(logging.Formatter('[%(levelname)s] [%(asctime)s] %(name)s: %(message)s'))
+    console_handler.setFormatter(
+        logging.Formatter("[KAREN] [%(levelname)s] [%(asctime)s] %(name)s: %(message)s")
+    )
     console_handler.setLevel(logging.DEBUG)
     root_logger.addHandler(console_handler)
-
