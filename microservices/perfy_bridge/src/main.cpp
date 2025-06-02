b9efc871ac65cadd74b78507a3e2e008fe04367f
Szymon Witt <szymonw@chalmers.se>
2025-06-02 12:14:37 +0200

    feat: add logger to the bridge

--- microservices/perfy_bridge/src/main.cpp
+++ microservices/perfy_bridge/src/main.cpp
@@ -1,4 +1,5 @@
 #include <cstdint>
+#include <cstring>
 #include <iostream>
 #include <unistd.h>
 #include <vector>
@@ -9,7 +10,9 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 
-enum HeaderParseStatus { 
+#include "logger.hpp"
+
+enum HeaderParseStatus {
     TERMINATION_SIGNAL = 0,
     FRAME_NOT_AVAILABLE = 1,
     FRAME_TOO_BIG = 2,
@@ -43,21 +46,23 @@ double read_double(const uint8_t* data) {
 }
 
 ParsedHeader parse_header(
-    uint8_t* data, 
-    const size_t max_frame_size 
+    uint8_t* data,
+    const size_t max_frame_size
 ) {
+    auto logger = Logger::get_instance().get_logger();
+
     uint8_t is_frame_available = data[0];
     uint8_t should_terminate = data[1];
 
     if (is_frame_available == 0 && should_terminate == 1) {
-        std::cout << "Termination signal received from python. Exiting the main loop." << std::endl;
+        logger->info("Termination signal received from python. Exiting the main loop.");
         return ParsedHeader{
             TERMINATION_SIGNAL,
             0,
             0.0,
             {},
         };
-    } 
+    }
     if (is_frame_available != 1) {
         usleep(1000);
         return ParsedHeader{
@@ -75,7 +80,7 @@ ParsedHeader parse_header(
 
     uint32_t content_length = read_uint32(data + offset_encoded_size);
     if (content_length > max_frame_size) {
-        std::cerr << "Received frame is too big: " << content_length << " bytes" << std::endl;
+        logger->error("Received frame is too big: {} bytes", content_length);
         data[0] = 0;
         return ParsedHeader{
             FRAME_TOO_BIG,
@@ -87,12 +92,12 @@ ParsedHeader parse_header(
 
     uint64_t frame_ts = read_uint64(data + offset_timestamp);
     double latest_steering = read_double(data + offset_steering);
-    
+
     std::vector<uint8_t> frame_data(content_length);
     std::memcpy(frame_data.data(), data + header_size, content_length);
 
     data[0] = 0;
-    
+
     return ParsedHeader{
         OK,
         frame_ts,
@@ -102,35 +107,40 @@ ParsedHeader parse_header(
 }
 
 int main(){
+    auto logger = Logger::get_instance().get_logger();
+
     // those values have to match values used by the python producer
     const key_t SHM_KEY = 0x696d67;
     const int HEADER_SIZE = 22;
     const int MAX_H264_FRAME_SIZE = 200 * 1024;
     const int SHARED_MEMORY_SIZE = HEADER_SIZE + MAX_H264_FRAME_SIZE;
-    
+
+    std::stringstream ss;
+    ss << std::hex << SHM_KEY;
+
     // Python shared memory setup
     int shm_id = shmget(SHM_KEY, SHARED_MEMORY_SIZE, 0666);
     if (shm_id == -1) {
-        std::cerr << "Error: Failed to get shared memory with key 0x" << std::hex << SHM_KEY << ": " << std::strerror(errno) << std::endl;
+        logger->error("Failed to get shared memory with key 0x{}: {}", ss.str(), std::strerror(errno));
         return 1;
     }
 
     void* shm_ptr= shmat(shm_id, nullptr, 0);
     if (shm_ptr == (void*)-1) {
-        std::cerr << "Error: Failed to attach to shared memory: " << std::strerror(errno) << std::endl;
+        logger->error("Failed to attach to shared memory: {}", std::strerror(errno));
         return 1;
     }
-    std::cout << "Attached to shared memory with key 0x" << std::hex << SHM_KEY << std::dec << std::endl;
+    logger->info("Attached to shared memory with key 0x{}", ss.str());
 
-    // Cluon shared memory setup 
+    // Cluon shared memory setup
     const int cluon_cid = 253;
     cluon::OD4Session od4(cluon_cid, [](cluon::data::Envelope &&envelope) noexcept {});
     if (!od4.isRunning()) {
-        std::cerr << "Failed to start cluon OD4 session with CID " << cluon_cid << std::endl;
+        logger->error("Failed to start cluon OD4 session with CID '{}'", cluon_cid);
         shmdt(shm_ptr);
         return 1;
     }
-    std::cout << "Started cluon OD4 session with CID " << cluon_cid << std::endl;
+    logger->info("Started cluon OD4 session with CID '{}'", cluon_cid);
 
     uint32_t frame_count = 0;
     while (true) {
@@ -147,7 +157,7 @@ int main(){
         cluon::data::TimeStamp cluon_ts;
         cluon_ts.seconds(static_cast<int32_t>(result.frame_ts / 1000000));
         cluon_ts.microseconds(static_cast<int32_t>(result.frame_ts % 1000000));
-        
+
         opendlv::proxy::ImageReading msg;
         msg.data(std::string(result.frame_data.begin(), result.frame_data.end())).fourcc("h264").width(640).height(480);
         od4.send(msg, cluon_ts, 0);
@@ -158,12 +168,13 @@ int main(){
 
         frame_count += 1;
         if (frame_count % 100 == 0) {
-            std::cout << "Received "<< std::dec << frame_count << " frames" << std::endl;
+            logger->debug("Received {} frames", frame_count);
         }
     }
-    
+    logger->info("Done. Received {} frames", frame_count);
+
+    logger->info("Detaching from shared memory with key {}", ss.str());
     shmdt(shm_ptr);
-    std::cout << "Done. Received " << frame_count << " frames " << std::endl;
 
     return 0;
 }
