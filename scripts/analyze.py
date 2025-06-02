eaaafb63c35ddd456c3834d4058bb7c4f4529184
Szymon Witt <szymonw@chalmers.se>
2025-06-02 15:03:41 +0200

    feat: add docker compose and python orchestration script

---
+++ scripts/analyze.py
@@ -0,0 +1,28 @@
+import uuid
+import subprocess
+import os
+
+RECORDINGS = [
+    "CID-140-recording-2020-03-18_144821-selection.rec",
+    # "CID-140-recording-2020-03-18_145043-selection.rec",
+    # "CID-140-recording-2020-03-18_145233-selection.rec",
+    # "CID-140-recording-2020-03-18_145641-selection.rec",
+    # "CID-140-recording-2020-03-18_150001-selection.rec",
+]
+
+# TODO: random for now, change later!
+COMMIT_ID = uuid.uuid4()
+
+for rec_file in RECORDINGS:
+    print(f"Running with recording file '{rec_file}'")
+
+    env = os.environ.copy()
+    env["RECORDING_FILE"] = rec_file
+    env["GIT_COMMIT_ID"] = str(COMMIT_ID)
+
+    subprocess.run([
+        "docker", "compose", "-f", "docker-compose.yml", "up", "--build", "--abort-on-container-exit"
+    ], check=True, env=env)
+
+    subprocess.run(["docker", "compose", "down", "--remove-orphans"], check=True)
+
