b09c4b1f289f590ab182566d0a51a168c86d8f9f
Szymon Witt <szymonw@chalmers.se>
2025-06-05 01:38:27 +0200

    refactor: replace print with logging

--- microservices/karen/src/main.py
+++ microservices/karen/src/main.py
@@ -1,11 +1,12 @@
 import argparse
+import logging
 
 from db.database import Sqlite 
-import os
 from core.logger import setup_logging
 from steering.controller import SteeringDataController
 from plotting.commit_comparison import generate_plots
 from video_processor import VideoProcessor
+
 
 def main() -> None:
     parser = argparse.ArgumentParser(
@@ -20,15 +21,15 @@ def main() -> None:
     args = parser.parse_args()
 
     setup_logging()
+    logger = logging.getLogger(__name__)
 
     db = Sqlite()
     data_controller = SteeringDataController()
     processor = VideoProcessor(db=db, data_controller=data_controller)
 
     processor.process(commit_id=args.commit_id, video_file_name=args.video_file)
-
-    print(">>>>> Finished processing, about to generate plots")
     
+    logger.info("Finished processing, about to generate plots")
     with db.new_session() as session:
         generate_plots(session=session)
 
