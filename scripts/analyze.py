30d32bc58186103a25d005018e164c7d58bdf340
Szymon Witt <szymonw@chalmers.se>
2025-06-05 10:17:10 +0200

    chore: analyze.py clean up

--- scripts/analyze.py
+++ scripts/analyze.py
@@ -1,33 +1,23 @@
-import uuid
 import subprocess
 import os
 import time
 
 RECORDINGS = [
     "CID-140-recording-2020-03-18_144821-selection.rec",
-    # "CID-140-recording-2020-03-18_145043-selection.rec",
-    # "CID-140-recording-2020-03-18_145233-selection.rec",
-    # "CID-140-recording-2020-03-18_145641-selection.rec",
-    # "CID-140-recording-2020-03-18_150001-selection.rec",
+    "CID-140-recording-2020-03-18_145043-selection.rec",
+    "CID-140-recording-2020-03-18_145233-selection.rec",
+    "CID-140-recording-2020-03-18_145641-selection.rec",
+    "CID-140-recording-2020-03-18_150001-selection.rec",
 ]
 
 COMMIT_ID = subprocess.check_output(["git", "rev-parse", "--short", "HEAD"]).decode().strip()
-#COMMIT_ID = uuid.uuid4()
 
 for rec_file in RECORDINGS:
     print(f"Running with recording file '{rec_file}'")
 
-    # env = os.environ.copy()
-    # env["RECORDING_FILE"] = rec_file
-    # env["GIT_COMMIT_ID"] = str(COMMIT_ID)
-
     with open(".env", "w") as f:
         f.write(f"RECORDING_FILE={rec_file}\n")
         f.write(f"GIT_COMMIT_ID={COMMIT_ID}\n")
-
-    #subprocess.run([
-    #    "docker", "compose", "-f", "docker-compose.yml", "up", "--build", "--exit-code-from", "karen"
-    #], check=True, env=env)
 
     compose_proc = subprocess.Popen([
         "docker", "compose", "-f", "docker-compose.yml", "up", "--build"
@@ -55,7 +45,6 @@ for rec_file in RECORDINGS:
         "docker", "compose", "-f", "docker-compose.yml", "down", "--remove-orphans"
     ], check=True) 
 
-    #subprocess.run(["docker", "compose", "down", "--remove-orphans"], check=True)
 
 os.remove(".env")
 
